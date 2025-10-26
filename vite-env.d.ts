724e55607bca1e5c3b70c8c79f517068a2116f7b
Hiro <laciferin@gmail.com>
2025-10-26 16:18:19 +0530

    eenv: VITE_INFURA_API_KEY

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_MONEY_AUTH_URL: string
   readonly VITE_VERIFIER_API_URL: string
   readonly VITE_WALLETCONNECT_PROJECT_ID: string
+  readonly VITE_INFURA_API_KEY: string
 }
 
 interface ImportMeta {
