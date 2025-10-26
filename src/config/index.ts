9b98731bdc76fae70b0a1d1c5143e9ba8d1dc1a4
Nasfame <laciferin2024@protonmail.com>
2025-10-26 19:31:19 +0530

    nits:

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const DEMO_VIDEO_URL = "https://youtu.be/idCJSmm6QUs"
+export const DEMO_VIDEO_URL = "https://youtu.be/5XN1MwFJ6Ks"
 
 export const VERIFIER_API_URL =
   import.meta.env.VITE_VERIFIER_API_URL || "https://auth.money-pot.unreal.art"
